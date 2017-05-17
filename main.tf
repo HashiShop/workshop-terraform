8604da2db637092131d923214d596ab407767e53
VEBER Arnaud <arnaud@veber.pw>
2017-05-17 14:18:53 +0200

    feat(tf): security group http_in

--- main.tf
+++ main.tf
@@ -11,3 +11,29 @@ provider "aws" {
     secret_key = "${var.AWS_SECRET_KEY}"
     region = "${var.AWS_REGION}"
 }
+
+# Resources
+## Security Groups
+resource "aws_security_group" "http_in" {
+    name = "http in"
+    description = "Allow http inbound traffic"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags {
+        Name = "http in"
+        Environment = "${terraform.env}"
+    }
+}
