23da6113d5d7d42cb78dc359eb7d0257feb2ed98
VEBER Arnaud <arnaud@veber.pw>
2017-05-17 15:48:58 +0200

    feat(tf): remove amazon_linux

--- amazon_linux.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "amzn_linux" {
-    ami = "${lookup(var.amis, "amzn_linux")}"
-    instance_type = "t2.micro"
-
-    tags {
-        Name = "amazon linux"
-        Environment = "${terraform.env}"
-    }
-}
