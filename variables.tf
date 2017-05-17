eb2e1b22d3042173aafcac09dcc46cb41992e1f8
VEBER Arnaud <arnaud@veber.pw>
2017-05-17 15:51:20 +0200

    feat(tf): remove unnecessary variables

--- variables.tf
+++ variables.tf
@@ -3,10 +3,3 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
     default = "eu-west-1"
 }
-
-variable "amis" {
-    default = {
-        "hextris" = "ami-627a7f04"
-        "amzn_linux" = "ami-df99a4b9"
-    }
-}
