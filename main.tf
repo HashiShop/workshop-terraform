bb888072096ac417af2fc95d573c91a2633c654b
VEBER Arnaud <arnaud@veber.pw>
2017-05-17 15:15:11 +0200

    feat(tf): set ami ids as variable

--- main.tf
+++ main.tf
@@ -4,6 +4,12 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
     default = "eu-west-1"
 }
+variable "amis" {
+    default = {
+        "hextris" = "ami-627a7f04"
+        "amzn_linux" = "ami-df99a4b9"
+    }
+}
 
 # Providers
 provider "aws" {
