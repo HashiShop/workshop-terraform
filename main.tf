7ff344b027fa6680f57874f50085c3636e9d7649
VEBER Arnaud <arnaud@veber.pw>
2017-05-17 14:18:22 +0200

    feat(tf): provider

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
     default = "eu-west-1"
 }
+
+# Providers
+provider "aws" {
+    access_key = "${var.AWS_ACCESS_KEY}"
+    secret_key = "${var.AWS_SECRET_KEY}"
+    region = "${var.AWS_REGION}"
+}
